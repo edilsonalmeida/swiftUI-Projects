aee71073f40d2d765406f35660658698f5c1446f
Edilson Almeida <contato.edilsonalmeida@gmail.com>
2024-03-29 03:07:04 -0300

    Text View
    New project about Text View

---
+++ TextViewProperties/TextViewProperties/TextViewPropertiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextViewPropertiesApp.swift
+//  TextViewProperties
+//
+//  Created by edilsonalmeida on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TextViewPropertiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
